66589c8476758ba541ce16ec0d58968f572f57aa
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-11-25 15:26:58 +0530

    app completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Notes from "./pages/Notes";
+import Tasks from "./pages/Tasks";
+import store from "./store/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="notes" element={<Notes />} />
+            <Route path="tasks" element={<Tasks />} />
+          </Route>
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
